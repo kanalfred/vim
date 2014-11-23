c4196ca5a076595fcc6329cbfa9242a5fcb1b2ef
Alfred <kanalfred@gmail.com>
2014-11-22 17:02:32 -0800

    update README for better ctags

--- update-better-ctags.sh
+++ update-better-ctags.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/better-ctags/ctags -R --fields=+aimS --langmap=php:.class.inc.php --languages=php --php-kinds=cdfi
+~/ctags-better/ctags -R --fields=+aimS --langmap=php:.class.inc.php --languages=php --php-kinds=cdfi

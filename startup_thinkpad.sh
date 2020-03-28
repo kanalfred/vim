a56ce35837e7447b9bf9ff01156642cb029f0d79
Alfred <kanalfred@gmail.com>
2020-03-28 16:53:28 -0700

    add thinkpad startup script - set trackpad

---
+++ startup_thinkpad.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Profile Enabled' 0, 1 2> /dev/null
+xinput set-prop "TPPS/2 IBM TrackPoint" 'libinput Accel Speed' 0.5 2> /dev/null
